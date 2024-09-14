126854d6d60ef65fe72d7f48118cb39feb0a33ba
Khoa Nguyen <khoanguyen1799x@gmail.com>
2024-09-14 11:58:34 +0700

    test run husky

--- src/index.js
+++ src/index.js
@@ -1,44 +1,34 @@
-const handlebars = require("express-handlebars");
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
+const handlebars = require('express-handlebars');
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-app.use(morgan("combined"));
+const route = require('./routes');
+
+app.use(morgan('combined'));
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // template engine
 app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get("/", function (req, res) {
-  res.render("home");
-});
-app.get("/news", function (req, res) {
-  res.render("news");
-});
-app.get("/search", function (req, res) {
-  console.log(req.query.q);
-  res.render("search");
-});
-app.post("/news", function (req, res) {
-  console.log(req.body.news);
-  res.send("");
-});
+// Routes init
+route(app);
 
 app.listen(
-  port,
-  console.log(`Example app listening at http://localhost:${port}`)
+    port,
+    console.log(`Example app listening at http://localhost:${port}`),
 );
---
+++ src/routes/site.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const siteController = require('../app/controller/SiteController');
+express.urlencoded({
+    extended: true,
+});
+express.json();
+router.post('/search', function (req, res) {
+    console.log('post');
+    console.log('Request: ', req.body);
+    res.send('');
+});
+router.get('/search', function (req, res) {
+    console.log('get');
+    console.log('Request: ', req.query.q);
+
+    res.render('search');
+});
+router.use('/search', siteController.search);
+router.use('/', siteController.home);
+module.exports = router;
